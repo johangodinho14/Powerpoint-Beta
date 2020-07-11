db5a1b8684d7d3e357951026226aa0f331c810a8
johangodinho14 <johangodinho14@gmail.com>
2020-07-11 23:13:50 +0100

    added functionality to left and right slide control buttons/ images

--- viewerApp.js
+++ viewerApp.js
@@ -1,28 +1,33 @@
-const slideContentHandler = ()=>{
+let i               = 1;
+let numOfSlides;
 
+const slideContentHandler = ()=>{
     fetch("./source.json")
     .then((resp)=>{
         return resp.json();
     })
     .then((data)=>{
-        const numOfSlides   = 1;
+        numOfSlides             = Object.keys(data).length;
         //Reads through all the slides present
-        for(let i=1;i<numOfSlides+1;i++){
             let docContent      = document.getElementById('content');
             let docTitle        = document.getElementById('title');
+            let docSlideNum     = document.getElementById('slideNumber');
+            docContent.innerHTML= "";
+            docTitle.innerHTML  = "";
 
             let currentSlide    = 'slide'+i;
             let slideContentlen = (data[currentSlide].length);
             let slideProperties;
             let slideContent;
-            let strippedSlideContent;
-            console.log(currentSlide)
+
+            docSlideNum.innerText="Slide Number : ";
+            docSlideNum.innerText+= +" "+i;
+
             //Reads through the content of the slides individually
                 for(let i=0;i<slideContentlen;i++){
                     slideProperties     = (Object.keys(data[currentSlide][i]));
                     slideProperties     = slideProperties.toString();
                     slideContent        = data[currentSlide][i][slideProperties];
-                    strippedSlideContent=   slideContent.split(" ");
 
                     if(slideProperties === 'title'){
                         docTitle.innerText = slideContent;
@@ -35,12 +40,41 @@ const slideContentHandler = ()=>{
                         docContent.innerHTML += '<img '+'src='+slideContent+'>';
                     }
                 }
-        }
+        
     })
 
    
 }
 
+const slideControllers    = ()=>{
+    const leftArrow       = document.getElementById('leftArrow');
+    const rightArrow      = document.getElementById('rightArrow');
+    const notification    = document.getElementById('notification');
+
+    leftArrow.addEventListener('click',()=>{
+        if(i===1){
+            i=1; 
+            notification.innerHTML="No more slides to display";
+            slideContentHandler();
+            return; 
+        };
+        i -= 1;
+        slideContentHandler();
+    });
+
+    rightArrow.addEventListener('click',()=>{
+        if(i===numOfSlides){
+            i=1; 
+            notification.innerHTML="No more slides to display";
+            slideContentHandler();
+            return; 
+        };
+        i += 1;
+        slideContentHandler();
+    });
+
+}
 window.addEventListener('load', ()=>{
     slideContentHandler();
+    slideControllers();
 })
